f2fd4b96019f1ffae36db24428543c1c5e60aef7
Olga <lili432@mail.ru>
2018-05-05 13:09:03 +0300

    settings (react в вебпаке)

---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,32 @@
+let webpack = require('webpack');
+let path = require('path');
+
+let parentDir = path.join(__dirname, '../');
+let src = path.join(__dirname, '../src');
+
+module.exports = {
+    entry: [
+        path.join(src, 'index.js')
+    ],
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        },{
+            test: /\.less$/,
+            loaders: ["style-loader", "css-loder", "less-loader"]
+        }
+        ]
+    },
+    mode: 'development',
+    output: {
+        path: parentDir + '/dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: parentDir,
+        compress: true,
+        port: 8080
+    }
+}
