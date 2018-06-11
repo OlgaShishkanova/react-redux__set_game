7b510152ee2bb5806cc156a7774df98e6061282b
Olga <lili432@mail.ru>
2018-06-11 09:51:30 +0300

    settings

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,6 +4,23 @@ import './App.scss';
 export default class App extends Component {
 
     render () {
-        return <div className="puka">potato</div>
+        return <div className="puka">potato
+            <div>fsfs
+                fg
+                df
+                uio;io;i
+                bh
+                d</div>
+            <div>
+                tyjtyj
+                tyj
+                ty
+                jt
+                y
+                jt
+                yj
+
+            </div>
+        </div>;
     }
 }
